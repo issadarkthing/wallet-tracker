770e5867128027d0f7b21612b5dc8c9264492339
raziman <raziman.terra@gmail.com>
2024-02-09 17:57:49 +0800

    Add duration as option

--- src/main.ts
+++ src/main.ts
@@ -93,6 +93,41 @@ function priceChangeIndicator(price: number) {
     }
 }
 
+// convert "1h", "3m", "4d" to milliseconds
+function parseDuration(interval: string) {
+    const second = 1000;
+    const minute = 60 * second;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+    const week = 7 * day;
+    const supportedDurations = ["s", "m", "h", "d", "w"];
+    const amount = Number(interval.match(/^\d+/)?.at(0));
+    const duration = interval.match(/\w$/)?.at(0);
+
+    if (!amount || !duration) {
+        throw new Error("invalid duration");
+    }
+
+    if (!supportedDurations.includes(duration)) {
+        throw new Error("invalid duration");
+    }
+
+    switch (duration) {
+        case "s":
+            return second * amount;
+        case "m":
+            return minute * amount;
+        case "h":
+            return hour * amount;
+        case "d":
+            return day * amount;
+        case "w":
+            return week * amount;
+        default:
+            return 12 * hour;
+    }
+}
+
 bot.onText(/\/status/, async () => {
     console.log("STATUS command executed");
     const data = await getData();
@@ -124,15 +159,12 @@ bot.onText(/\/start/, () => {
     });
 });
 
-setInterval(
-    async () => {
-        console.log("crypto portfolio update");
-        const data = await getData();
-        const chatId = process.env.CHAT_ID!;
+setInterval(async () => {
+    console.log("crypto portfolio update");
+    const data = await getData();
+    const chatId = process.env.CHAT_ID!;
 
-        bot.sendMessage(chatId, data, { parse_mode: "HTML" });
-    },
-    60 * 60 * 1000
-);
+    bot.sendMessage(chatId, data, { parse_mode: "HTML" });
+}, parseDuration(process.env.DURATION!));
 
 console.log("bot started");
